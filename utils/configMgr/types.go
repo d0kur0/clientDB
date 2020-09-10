8e75701b432e97dc3ee13b86fb07c3df73b7c0b8
d0kur0 <dev.dokuro@gmail.com>
2020-09-10 23:44:56 +0300

    add redis configuration type

--- utils/configMgr/types.go
+++ utils/configMgr/types.go
@@ -1,13 +1,20 @@
 package configMgr
 
-type MysqlConfig struct {
-	Host string `json:"host"`
-	User string `json:"user"`
-	Password string `json:"password"`
-	Dbname string `json:"dbname"`
+type ServerConfig struct {
+	Port  int         `json:"port"`
+	Mysql MysqlConfig `json:"mysql"`
+	Redis RedisConfig `json:"redis"`
 }
 
-type ServerConfig struct {
-	Port int `json:"port"`
-	Mysql MysqlConfig `json:"mysql"`
+type MysqlConfig struct {
+	Host     string `json:"host"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Dbname   string `json:"dbname"`
+}
+
+type RedisConfig struct {
+	Host     string `json:"host"`
+	Password string `json:"password"`
+	Dbname   int    `json:"dbname"`
 }
