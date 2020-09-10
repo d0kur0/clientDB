484e023829af95fabfa2911aaffee52dcb621f7b
d0kur0 <dev.dokuro@gmail.com>
2020-09-10 23:42:24 +0300

    remove useless logs, add redis and gorm connection

--- main.go
+++ main.go
@@ -3,13 +3,32 @@ package main
 import (
 	"log"
 
+	"github.com/d0kur0/clientDB/redisWrapper"
+
 	"github.com/d0kur0/clientDB/router"
+
+	"github.com/d0kur0/clientDB/dbWrapper"
 
 	"github.com/d0kur0/clientDB/utils/configMgr"
 )
 
 func main() {
 	if err := configMgr.Parse(); err != nil {
+		log.Println(err)
+		return
+	}
+
+	if err := dbWrapper.Init(); err != nil {
+		log.Println(err)
+		return
+	}
+
+	if err := dbWrapper.Migrate(); err != nil {
+		log.Println(err)
+		return
+	}
+
+	if err := redisWrapper.Init(); err != nil {
 		log.Println(err)
 		return
 	}
