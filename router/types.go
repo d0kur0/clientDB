7cfc36b0f8640fd6501574ee43214020ca7d27d1
d0kur0 <dev.dokuro@gmail.com>
2020-09-10 23:44:33 +0300

    add new field for set access type for route

--- router/types.go
+++ router/types.go
@@ -3,7 +3,8 @@ package router
 import "net/http"
 
 type Route struct {
-	Methods []string
-	Path string
-	Handler func (w http.ResponseWriter, r *http.Request)
+	Methods  []string
+	Path     string
+	Handler  func(w http.ResponseWriter, r *http.Request)
+	AuthNeed bool
 }
